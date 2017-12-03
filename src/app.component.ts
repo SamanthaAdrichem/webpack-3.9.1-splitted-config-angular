a46a066278af01642bd968a8ccde92c18602e2be
SamanthaAdrichem <samantha@adrichem.nu>
2017-12-03 16:43:13 +0100

    Added style url's to show those work aswell

--- src/app.component.ts
+++ src/app.component.ts
@@ -3,6 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
 	selector: 'my-app',
 	templateUrl: 'templates/app.html',
+	styleUrls: ['styles/app.scss'],
 	encapsulation: ViewEncapsulation.None
 })
 export class AppComponent { }
