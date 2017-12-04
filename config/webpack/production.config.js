c55315a7f011e956d365ae5c5272884064e365f2
Samantha Adrichem <samantha@adrichem.nu>
2017-12-04 18:11:17 +0100

    Added package-lock update, fix for production htaccess

--- config/webpack/production.config.js
+++ config/webpack/production.config.js
@@ -1,12 +1,17 @@
+const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 import { commonConfig } from './common.config';
+import { srcPath } from "../vars/paths.config";
 
 export let productionConfig;
 
 productionConfig = {
 	plugins: [
+		new CopyWebpackPlugin([ { from: '.htaccess' } ], { context: srcPath } ),
+		new webpack.LoaderOptionsPlugin({ minimize: true }),
 		new UglifyJsPlugin({
 			sourceMap: true
 		})
