fb0f9760e1ea7b4db5c0ade6b364d63f1bbd2bfc
SamanthaAdrichem <samantha@adrichem.nu>
2017-12-07 16:25:51 +0100

    Removed package sort in favor of CommonChunks

--- config/webpack/loaders/template-loader.config.js
+++ config/webpack/loaders/template-loader.config.js
@@ -28,24 +28,7 @@ export const templateLoader = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: srcPath + '/index.html',
-			chunksSortMode: packageSort(['polyfills', 'vendor', 'app'])
+			template: srcPath + '/index.html'
 		})
 	]
 };
-
-function packageSort(packages) {
-	return function sort(left, right) {
-		let leftIndex = packages.indexOf(left.names[0]);
-		let rightIndex = packages.indexOf(right.names[0]);
-
-		if ( leftIndex < 0 || rightIndex < 0) {
-			throw 'unknown package';
-		}
-
-		if (leftIndex > rightIndex){
-			return 1;
-		}
-		return -1;
-	}
-}
